ceacf6fd32c3e023dd096e3118374652193e0932
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2024-02-23 13:15:16 -0600

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-black">
+      <p className="text-xl">Click on the Vite and React logos to learn more</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-dupe-keys */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      xs: "374px",
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      /* Primary */
+      brightBlue: "hsl(220, 98%, 61%)",
+      checkBackground:
+        "linear-gradient hsl(192, 100%, 67%) to hsl(280, 87%, 65%)",
+
+      /* Light Theme */
+      veryLightGray: "hsl(0, 0%, 98%)",
+      veryLightGrayishBlue: "hsl(236, 33%, 92%)",
+      lightGrayishBlue: "hsl(233, 11%, 84%)",
+      darkGrayishBlue: "hsl(236, 9%, 61%)",
+      veryDarkGrayishBlue: "hsl(235, 19%, 35%)",
+
+      /* Dark Theme */
+      veryDarkBlue: "hsl(235, 21%, 11%)",
+      veryDarkDesaturatedBlue: "hsl(235, 24%, 19%)",
+      lightGrayishBlue: "hsl(234, 39%, 85%)",
+      lightGrayishBlueHover: " hsl(236, 33%, 92%)",
+      darkGrayishBlue: "hsl(234, 11%, 52%)",
+      veryDarkGrayishBlue: "hsl(237, 14%, 26%)",
+    },
+    fontFamily: {
+      sans: ["Josefin Sans"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
