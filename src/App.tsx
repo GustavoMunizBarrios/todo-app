da6b32322160a0b0415e5e2a1086d7196f1da8bd
Gustavo Muñiz <j.gustavomb1984@hotmail.com>
2024-02-26 19:18:23 -0600

    I added styles and components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import bgMobileDark from "./images/bg-mobile-dark.jpg";
+import iconSun from "./images/icon-sun.svg";
+
+import NewTask from "./components/NewTask";
+import TaskList from "./components/TaskList";
+import SortTasks from "./components/SortTasks";
+
 export default function App() {
   return (
-    <div>
-      <h1 className="text-lightGrayishBlue font-bold text-2xl">TODO</h1>
+    <div className="relative flex flex-col h-screen text-center items-center">
+      <div className="flex w-screen justify-between items-center pt-11 pr-5 pl-5 pb-8">
+        <h1 className="text-lightGrayishBlue font-bold text-3xl tracking-[10px]">
+          TODO
+        </h1>
+        <button>
+          <img className="w-[25px]" src={iconSun} alt="Sun icon" />
+        </button>
+      </div>
+
+      <img
+        className="absolute top-0 -z-40"
+        src={bgMobileDark}
+        alt="Background dark"
+      />
+      <NewTask />
+      <TaskList />
+      <SortTasks />
       <p className="text-darkGrayishBlue text-base font-medium">
         Drag and drop to reorder list
       </p>
---
+++ src/components/NewTask.tsx
@@ -0,0 +1,23 @@
+export default function NewTask() {
+  return (
+    <>
+      <div
+        className="flex items-center justify-start 
+      w-[21rem] h-12
+      bg-veryDarkDesaturatedBlue rounded-md
+      pl-4"
+      >
+        <div className="w-5 h-5 border border-veryDarkGrayishBlue rounded-full "></div>
+        <form className="pl-4 text-veryDarkGrayishBlue">
+          <input
+            className="bg-veryDarkDesaturatedBlue 
+            outline-none focus:outline-none
+            focus:text-lightGrayishBlueHover text-sm font-normal"
+            type="text"
+            placeholder="Create a new todo..."
+          />
+        </form>
+      </div>
+    </>
+  );
+}
